e10c3010176759d5f27d97f1c66768bb25bbbbff
gabrielbomtempo <gasopcanadianguy@gmail.com>
2025-10-27 13:17:34 -0300

    init commit

--- src/main/java/bank/Bank.java
+++ src/main/java/bank/Bank.java
@@ -5,23 +5,24 @@ import java.util.ArrayList;
 public class Bank {
     private final ArrayList<Account> accounts;
 
-    public Bank () {
+    public Bank() {
         accounts = new ArrayList<>();
     }
-   public Account findAccount(String username) {
+
+    public Account findAccount(String username) {
         for (Account x : accounts) {
             if (x.getUsername().equalsIgnoreCase(username)) {
                 return x;
             }
         }
-    return null;
+        return null;
     }
-    public boolean createAccount(String username, String password, double balance) {
+
+    public void createAccount(String username, String password, double balance) {
         if (findAccount(username) != null) {
-            return false;
+            return;
         }
-        Account newAccount = new Account(username,password,balance);
+        Account newAccount = new Account(username, password, balance);
         accounts.add(newAccount);
-        return true;
     }
 }
